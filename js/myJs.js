e65c54d8a76730e982bc3878d67dec4ababa9a52
mindyp10101 <mindyp10101@gmail.com>
2023-02-12 09:03:31 -0500

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -9,7 +9,7 @@ const textConfig = {
   text8: "Gởi MP result nào LOL",
   text9: "Tại nhỏ MP nào đó vừa lười, vừa không ngoan, lại còn trẻ con, không có gì tốt hết!",
   text10: "Hiểu luôn!! Thế thôi MP xin lỗi ạ, từ nay không phiền Kiệt nữa ạ!!!",
-  text11: "P.S: Đùa chứ, Mp vẫn sẽ phiền bạn. Đến lúc nào đó, thang điểm thích Kiệt về số 0 thì sẽ không phiền nữa ạ. Cũng sắp rồi ý, nên Kiệt chịu khó 1 time nữa nha LOL. Coi như Kiệt xui đi ah!!",
+  text11: "P.S: Đùa chứ, Mp vẫn sẽ phiền bạn. Đến lúc nào đó, thang điểm thích Kiệt về số 0 thì sẽ không phiền nữa ạ. Kiệt chịu khó 1 time nữa nha, cũng sắp hết thích Kiệt rồi ạ LOL",
   text12: "Happy Val linh tinh, Kiệt!!",
 };
 
